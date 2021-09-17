01c17b7a58bafd80f220dfdf6f32fd2200a65982
Jacob Renn <jacob.renn@theaisquaredinc.onmicrosoft.com>
2021-09-17 09:45:07 -0400

    updating experiment 2

--- BothMNISTconv.py
+++ BothMNISTconv.py
@@ -142,6 +142,90 @@ if __name__ == '__main__':
     print('Dedicated Model Fashion Performance:')
     print(confusion_matrix(fashion_y_test, fashion_preds))
     print(classification_report(fashion_y_test, fashion_preds))
+
+    digit_input = tf.keras.layers.Input(digit_x_train.shape[1:])
+    fashion_input = tf.keras.layers.Input(fashion_x_train.shape[1:])
+    x = mann.layers.MultiMaskedConv2D(
+        32,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )([digit_input, fashion_input])
+    x = mann.layers.MultiMaskedConv2D(
+        32,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    digit_selector1 = mann.layers.SelectorLayer(0)(x)
+    fashion_selector1 = mann.layers.SelectorLayer(1)(x)
+    digit_maxpool1 = tf.keras.layers.MaxPool2D(
+        pool_size = 2,
+        strides = 1,
+        padding = 'valid'
+    )(digit_selector1)
+    fashion_maxpool1 = tf.keras.layers.MaxPool2D(
+        pool_size = 2,
+        strides = 1,
+        padding = 'valid'
+    )(fashion_selector1)
+    x = mann.layers.MultiMaskedConv2D(
+        64,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )([digit_maxpool1, fashion_maxpool1])
+    x = mann.layers.MultiMaskedConv2D(
+        64,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    digit_selector2 = mann.layers.SelectorLayer(0)(x)
+    fashion_selector2 = mann.layers.SelectorLayer(1)(x)
+    digit_maxpool2 = tf.keras.layers.MaxPool2D(
+        pool_size = 2,
+        strides = 1,
+        padding = 'valid'
+    )(digit_selector2)
+    fashion_maxpool2 = tf.keras.layers.MaxPool2D(
+        pool_size = 2,
+        strides = 1,
+        padding = 'valid'
+    )(fashion_selector2)
+    digit_flatten = tf.keras.layers.Flatten()(digit_maxpool2)
+    fashion_flatten = tf.keras.layers.Flatten()(fashion_maxpool2)
+    x = mann.layers.MultiMaskedDense(256, activation = 'relu')([digit_flattn, fashion_flatten])
+    x = mann.layers.MultiMaskedDense(256, activation = 'relu')(x)
+    output_layer = mann.layers.MultiMaskedDense(10, activation = 'softmax')(x)
+    model.compile(
+        loss = 'sparse_categorical_crossentropy',
+        metrics = ['accuracy'],
+        optimizer = 'adam'
+    )
+    model = mann.utils.mask_model(
+        model,
+        90,
+        [digit_x_train, fashion_x_train],
+        [digit_y_train, fashion_y_train]
+    )
+    model.compile(
+        loss = 'sparse_categorical_crossentropy',
+        metrics = ['accuracy'],
+        optimizer = 'adam'
+    )
+
+    model.fit(
+        [digit_x_train, fashion_x_train],
+        [digit_y_train, fashion_y_train],
+        epochs = 100,
+        batch_size = 512,
+        callbacks = [callback]
+    )
+
+    preds = model.predict([digit_x_test, fashion_x_test])
+    digit_preds = preds[0].argmax(axis = 1)
+    fashion_preds = preds[0].argmax(axis = 1)
     
     print('Multitask Model Digit Performance:')
     print(confusion_matrix(digit_y_test, digit_preds))

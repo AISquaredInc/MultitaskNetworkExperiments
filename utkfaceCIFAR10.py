8bf680fb20bf7788c41313b4b2390a4a569bce94
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-05-25 14:17:33 -0400

    fixing binary classification

--- utkfaceCIFAR10.py
+++ utkfaceCIFAR10.py
@@ -186,7 +186,7 @@ def main(train_dir, val_dir, batch_size, limit):
             print(x.shape)
         
         age_preds.append(new_preds[0].argmax(axis = 1).flatten().tolist())
-        gender_preds.append(new_preds[1].argmax(axis = 1).flatten().tolist())
+        gender_preds.append((new_preds[1] >= 0.5).flatten().tolist())
         ethnicity_preds.append(new_preds[2].argmax(axis = 1).flatten().tolist())
         cifar_preds.append(new_preds[3].argmax(axis = 1).flatten().tolist())
 
